909ad81795652a5af9ca0cea818886781d0f3946
Elijan Mastnak <git@ejmastnak.com>
2024-05-07 17:05:51 +0200

    feat: add overwrite flag to install script
    When overwrite flag is specified, script will overwrite existing files
    if present.
    
    This commit also refactors script to loop through an array of dotfile,
    instead of hardcodedly repeating logic for each dotfile.

--- install.sh
+++ install.sh
@@ -1,17 +1,59 @@
 #!/bin/bash
-# A simple shell script used to link dotfiles into place. 
-# Important: should be run from within dotfiles directory.
+# NAME
+#     install.sh - Link dotfiles into place.
+# 
+# SYNOPSIS
+#     install.sh [-o]
+#
+# A simple shell script used to link dotfiles into place.
+# IMPORTANT: the script should be run from within dotfiles directory.
 
-BASEDIR="$(pwd)"
+# Get the directory containing the script and convert to absolute path
+BASEDIR=$(dirname "${BASH_SOURCE[0]}")
+BASEDIR=$(cd "${BASEDIR}" && pwd)
+
+overwrite=0
+
+while getopts ":o" flag
+do
+  case "${flag}" in
+    o) overwrite=1;;
+    \?) # Invalid option
+      echo "Usage: install.sh [-o]"
+      exit;;
+  esac
+done
+
+dirs=("config" "vim")
+files=("bash_profile" "bashrc" "inputrc")
 
 # Link directories
-[[ ! -d "${HOME}/.config" ]]       && ln -s ${BASEDIR}/config ~/.config || echo '~/.config exists; skipping and not overwriting.'
-[[ ! -d "${HOME}/.vim" ]]          && ln -s ${BASEDIR}/vim ~/.vim; mkdir ~/.vim/plugged || echo '~/.vim exists; skipping and not overwriting.'
+for dir in "${dirs[@]}"; do
+  if [[ -d "${HOME}/.${dir}" || -L "${HOME}/.${dir}" ]]; then
+    if [[ ${overwrite} -eq 1 ]]; then
+      rm -rf "${HOME}/.${dir}"
+      ln -s ${BASEDIR}/${dir} "${HOME}/.${dir}"
+    else
+      echo "~/.${dir} exists; skipping and not overwriting."
+    fi
+  else
+    ln -s ${BASEDIR}/${dir} "${HOME}/.${dir}"
+  fi
+done
 
 # Link stand-alone files not in XDG_CONFIG
-[[ ! -e "${HOME}/.bash_profile" ]] && ln -s ${BASEDIR}/bash_profile ~/.bash_profile || echo '~/.bash_profile exists; skipping and not overwriting.'
-[[ ! -e "${HOME}/.bashrc" ]]       && ln -s ${BASEDIR}/bashrc ~/.bashrc || echo '~/.bashrc exists; skipping and not overwriting.'
-[[ ! -e "${HOME}/.inputrc" ]]      && ln -s ${BASEDIR}/inputrc ~/.inputrc || echo '~/.inputrc exists; skipping and not overwriting.'
+for file in "${files[@]}"; do
+  if [[ -e "${HOME}/.${file}" || -L "${HOME}/.${file}" ]]; then
+    if [[ ${overwrite} -eq 1 ]]; then
+      rm "${HOME}/.${file}"
+      ln -s ${BASEDIR}/${file} "${HOME}/.${file}"
+    else
+      echo "~/.${file} exists; skipping and not overwriting."
+    fi
+  else
+    ln -s ${BASEDIR}/${file} "${HOME}/.${file}"
+  fi
+done
 
 # Try installing Vim plugins (if Vim is installed and executable)
-[[ -x "$(command -v vim)" ]] && vim -c "PlugInstall|q"
+[[ -x "$(command -v vim)" ]] && vim -c "PlugInstall|quitall"
